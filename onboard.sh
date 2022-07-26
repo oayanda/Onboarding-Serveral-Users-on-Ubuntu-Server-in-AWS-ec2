f9e2d377ae98c7878e83d887461f5feb8a608499
Oluwabamidele Ayanda <oluwabamideleayanda@gmail.com>
2022-07-26 19:25:07 +0100

    completed

---
+++ onboard.sh
@@ -0,0 +1,64 @@
+# Requirements
+# Required folder - Shell - to hold all directories
+# Required files - names.csv id_rsa.pub onboarding_users.sh
+# Create a developers group
+
+
+#!/bin/bash
+userfile=$(cat names.csv)
+PASSWORD=password
+
+# To ensure the user running this script has sudo privilege
+    if [ $(id -u) -eq 0 ]; then
+
+# Reading the CSV file
+	for user in $userfile;
+	do
+            echo $user
+        if id "$user" &>/dev/null
+        then
+            echo "User Exist"
+        else
+
+# This will create a new user
+        useradd -m -d /home/$user -s /bin/bash -g developers $user
+        echo "New User Created"
+        echo
+
+
+# This will create a ssh folder in the user home folder
+        su - -c "mkdir ~/.ssh" $user
+        echo ".ssh directory created for new user"
+        echo
+
+# We need to set the user permission for the ssh dir
+         su - -c "chmod 700 ~/.ssh" $user
+         echo "user permission for .ssh directory set"
+         echo
+
+# This will create an authorized-key file
+        su - -c "touch ~/.ssh/authorized_keys" $user
+        echo "Authorized Key File Created"
+        echo
+
+# We need to set permission for the key file
+        su - -c "chmod 600 ~/.ssh/authorized_keys" $user
+        echo "user permission for the Authorized Key File set"
+        echo
+
+# We need to create and set public key for users in the server
+        cp -R "/home/ubuntu/Shell/id_rsa.pub" "/home/$user/.ssh/authorized_keys"
+        echo "Copyied the Public Key to New User Account on the server"
+        echo
+        echo
+
+        echo "USER CREATED"
+
+# Generate a password.
+sudo echo -e "$PASSWORD\n$PASSWORD" | sudo passwd "$user" 
+sudo passwd -x 5 $user
+            fi
+        done
+    else
+    echo "Only Admin Can Onboard A User"
+    fi
--- project_aux_shell_scripting.md
+++ project_aux_shell_scripting.md
@@ -4,7 +4,60 @@ Create a project folder.
 ```bash
 mkdir Shell && cd Shell
 ```
-In the new directory, create csv file - ***names.csv***
-![alt text](./images/1.png)
+In the new directory, create the following require files - ***names.csv id_rsa.pub onboarding_users.sh***
+![alt text](./images/2.png)
 
-Open and populate with names, one entry per line.
+Open and populate with names.csv, one entry per line. Save and exit.
+![alt text](./images/33.png)
+
+Open and populate the id_rsa.pub file the public key. Save and exit.
+![alt text](./images/4.png)
+
+Add shell script to automate the onboarding of 20 users. Save and exit.
+![alt text](./images/5.png)
+
+
+Create the developers group
+```bash
+sudo groupadd developers
+```
+![alt text](./images/44.png)
+
+Make the shell script executable
+```bash
+sudo chmod +x onboarding_users.sh
+```
+![alt text](./images/22.png)
+
+Change user to super and run script.
+![alt text](./images/21.png)
+
+User are created successfully
+![alt text](./images/23.png)
+
+Verify the new users, 
+```bash
+cd ../..
+```
+```bash
+ls -la
+```
+![alt text](./images/25.png)
+
+From a window client using putty, Login as James.
+Prepare private using PUTTYGEN, copy the private key and save.
+![alt text](./images/31.png)
+
+Save as private Key
+![alt text](./images/32.png)
+
+Open PUTTY, click Auth inside the ssh tab, load the just created ppk file.
+![alt text](./images/35.png)
+
+Scroll to the top and click on Session and insert the public ip of the ubuntu server. Click open and accept the prompt
+![alt text](./images/36.png)
+
+Login as James
+![alt text](./images/40.png)
+
+Click to view **[Demo video](https://drive.google.com/file/d/15Rcy48DctwKd7UkBwIgBAdj88PXLiLAe/view?usp=sharing)**
